e8dabdeea09a239c928d2e655411caa79271ffad
awoomouse <107973554+awoomouse@users.noreply.github.com>
2022-07-12 20:01:58 +0500

    2_1_3_SpringBeans

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,35 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
-    private Animal animal;
+    @Qualifier("cat")
+    private Animal cat;
+    @Autowired
+    @Qualifier("dog")
+    private Animal dog;
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(cat.toString() + dog.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
     }
 }
